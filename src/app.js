a06a63049dba6b00e978ad9978dfe4483c88f590
Victor Mesquita <mesquitadev@gmail.com>
2019-07-26 19:51:19 -0300

    Iniciando Crud

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 'use strict';
 // Dependências do projeto
 const express = require('express');
+const bodyParser = require('body-parser');
 // Instância do Express
 const app = express();
+
+// Traduz o conteúdo da request para json 
+app.use(bodyParser.json());
+// Codificar as urls
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Setando as rotas da aplicação
 const router = express.Router(); 
 
@@ -13,8 +20,14 @@ let route = router.get('/', (req, res, next) => {
         version : "0.0.1"
     });
 } );
-// Definindo Uso da Rota acima
-app.use('/', route);
+
+let create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+} );
+
+
+// Definindo Uso das Rotas acima
+app.use('/products', create);
 
 
 module.exports = app;
