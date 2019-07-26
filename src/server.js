1b77ed0d84621512862e704f4bab53fa4e209c78
Victor Mesquita <mesquitadev@gmail.com>
2019-07-26 19:09:51 -0300

    Tratando Erros da Aplicação

--- src/server.js
+++ src/server.js
@@ -29,10 +29,21 @@ app.use('/', route);
 
 // Criando servidor
 server.listen(port);
+// Verifica quando há erro
+server.on('error', onError);
+// Verifica o debug para ver erros da aplicação no evento listening
+server.on('listening', onListening);
+
 console.log('Api Rodando na porta ' + port);
 
 
-// Função para normalizar a porta
+/** 
+ * @name : normalizePort
+ * @author : Victor Mesquita
+ * @date : 26/07/2019 18:30
+ * Função para normalizar a porta e retornar em número
+ * @param val -> Recebe o valor da porta
+**/
 function normalizePort(val) {
     // Converte o valor para inteiro
     const port = parseInt(val, 10);
@@ -46,4 +57,46 @@ function normalizePort(val) {
         return port;
     }
     return false;
+}
+/** 
+ * @name : onError
+ * @author : Victor Mesquita
+ * @date : 26/07/2019 18:50
+ * Função para tratar os erros das portas
+ * @param error -> Recebe o valor do erro do servidor -> gerado automaticamente
+**/
+function onError(error) {
+    if(error.syscall !== 'listen') {
+        throw error;
+    } 
+
+    const bind = typeof port === 'string' ? 
+        'Pipe ' + port :
+        'Port ' + port;
+    switch(error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/** 
+ * @name : onListening
+ * @author : Victor Mesquita
+ * @date : 26/07/2019 18:50
+ * Função para fazer o debug da aplicação
+**/
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on' + bind);
 }
