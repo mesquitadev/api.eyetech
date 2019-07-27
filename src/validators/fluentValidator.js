afa3260421e3e0efe3e262592c9737b0b494522c
Victor Mesquita <mesquitadev@gmail.com>
2019-07-27 08:11:33 -0300

    Finalizando FluentValidator

--- src/validators/fluentValidator.js
+++ src/validators/fluentValidator.js
@@ -34,4 +34,22 @@ ValidationContract.prototype.isFixedLen = (value, len, message) => {
     }
 }
 
-ValidationContract
+ValidationContract.prototype.isEmail = (value, message) => {
+    var reg = new RegExp(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/);
+    if(!reg.test(value)) {
+        errors.push({ message : message });
+    }
+}
+
+ValidationContract.prototype.errors = () => {
+    return errors;
+}
+
+ValidationContract.prototype.clear = () => {
+    errors = [];
+}
+ValidationContract.prototype.isValid = () => {
+    return errors.length == 0;
+}
+
+module.exports = ValidationContract;
