d4f113cef0208e133c75ed97ea9aa6819cc1ebe4
Victor Mesquita <mesquitadev@gmail.com>
2019-07-26 23:00:02 -0300

    Finalizando CRUD

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,63 @@
 'use strict';
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+exports.get = (req, res, next) => {
+    Product.find({ active : true }, 'title price slug').then(data=>{
+        res.status(200).send(data);
+    }).catch(e=>{
+        res.status(400).send(e);
+    });
+}
+/**
+ * @name : getBySlug
+ * @author : Victor Mesquita 
+ * Busca no Registro pelo Slug do produto
+ */
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({ 
+        slug : req.params.slug,
+        active : true 
+    }, 'title price slug tags'
+    ).then(data=>{
+        res.status(200).send(data);
+    }).catch(e=>{
+        res.status(400).send(e);
+    });
+}
 
+/**
+ * @name : getById
+ * @author : Victor Mesquita 
+ * Busca no Registro pelo ID do produto
+ */
+exports.getById = (req, res, next) => {
+    Product.findById(req.params.id).then(data=>{
+        res.status(200).send(data);
+    }).catch(e=>{
+        res.status(400).send(e);
+    });
+}
+
+/**
+ * @name : getByTag
+ * @author : Victor Mesquita 
+ * Busca no Registro pela tag do produto
+ */
+exports.getByTag = (req, res, next) => {
+    Product.find({
+        tags : req.params.tag
+    }, 'title description price slug tags').then(data=>{
+        res.status(200).send(data);
+    }).catch(e=>{
+        res.status(400).send(e);
+    });
+}
+
+/**
+ * @name : Post Data
+ * @author : Victor Mesquita
+ * Função que salva os dados do produto no Banco
+ */
 exports.post = (req, res, next) => {
     // Instância do Modelo, com os parâmetros da request
     var product = new Product(req.body);
@@ -20,15 +76,45 @@ exports.post = (req, res, next) => {
     
 };
 
+/**
+ * @name : Put Data
+ * @author : Victor Mesquita
+ * Função que Atualiza os dados do produto no Banco
+ */
 exports.put = (req, res, next) => {
-    //Pegando id da request
-    let id = req.params.id;
-    res.status(201).send({
-        id : id, 
-        item : req.body
-    });
+    Product.findByIdAndUpdate(req.params.id, {
+        $set : {
+            title : req.body.title,
+            description : req.body.description,
+            price : req.body.price
+        }
+    }).then(x => {
+        res.status(201).send({
+            message : 'Produto atualizado com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message : 'Falha ao atualizar produto',
+            data : e
+        })
+    })
 };
 
+/**
+ * @name : Delete Data
+ * @author : Victor Mesquita
+ * Função que Deleta o produto do Banco
+ */
 exports.delete = (req, res, next) => {
-    res.status(201).send(req.body);
+    Product.findOneAndRemove(req.body.id)
+    .then(x => {
+        res.status(201).send({
+            message : 'Produto removido com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message : 'Falha ao remover produto',
+            data : e
+        })
+    })
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,6 +4,16 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productController');
 
+// Get all items
+router.get('/', controller.get);
+// Get By Slug
+router.get('/:slug', controller.getBySlug);
+// Get By Id
+router.get('/admin/:id', controller.getById);
+// Get By Id
+router.get('/tags/:tag', controller.getByTag);
+
+
 // Adiciona Item
 router.post('/', controller.post);
 //Atualizando item
